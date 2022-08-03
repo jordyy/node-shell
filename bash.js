ea16451275b768e8ca855449c203519b341dec86
Jordy <jordynavalee@gmail.com>
2022-08-03 15:21:48 -0500

    step 3 I guess

--- bash.js
+++ bash.js
@@ -1,15 +1,25 @@
-// process.stdout.write('prompt > '); 
+process.stdout.write('\nprompt > '); 
 
-// process.stdin.on('data', (data) => {
-//     const cmd = data.toString().trim(); 
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim(); 
     
-//     if (cmd === 'pwd') { 
-//         process.stdout.write(process.cwd());
-//     }  
+    if (cmd === 'pwd') { 
+        const pwd = require('./pwd');
+        pwd();
+    } 
+
+    if (cmd === 'ls') { 
+        fs.readdir('./', 'utf8', (err, files) => {
+            if(err) {
+                throw err
+            } else {
+                const ls = require('./ls');
+                ls();
+                process.stdout.write("prompt > "); 
+            }
+        })
+    } 
+
     
-//     process.stdout.write('\nprompt > ');
-// });
-
-let pwd = require('./pwd')
-
-console.log(pwd)
+    process.stdout.write('\nprompt > ');  
+  });
--- pwd.js
+++ pwd.js
@@ -1,12 +1,3 @@
-module.exports = process.stdout.write('prompt > '); 
-
-process.stdin.on('data', module.exports = (data) => {
-    const cmd = data.toString().trim(); 
-    
-    if (cmd === 'pwd') { 
-        process.stdout.write(process.cwd());
-    }  
-    
-    process.stdout.write('\nprompt > ');
-  });
-
+module.exports = function() { 
+    process.stdout.write(process.cwd());
+}
